63e6c4be7f4bcbcae5f9ad3855c85f023411135f
thxwelchs <thxwelchs@gmail.com>
2020-08-25 20:44:02 +0900

    init

---
+++ solving/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[100000] = {};
+    int dp[100000] = {};
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    dp[0] = a[0];
+    int answer = dp[0];
+
+    for(int i = 1; i < n; i++) {
+        dp[i] = max(a[i] + dp[i - 1], a[i]);
+        answer = max(answer, dp[i]);
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
